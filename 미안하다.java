8af70567c6d26458c14f45326a73d57262b7225a
olseonjin <128763258+olseonjin@users.noreply.github.com>
2023-09-06 18:59:16 +0900

    Add files via upload

---
+++ 미안하다.java
@@ -0,0 +1,7 @@
+
+public class Sorry2030 {
+	public static void main(String[] args) {
+	    int n = 2030;
+	    System.out.println("Sorry~~\n재밌어서 미안합니다~~");
+	}
+}
---
+++ 방갑다.java
@@ -0,0 +1,7 @@
+
+public class Hello2030 {
+    public static void main(String[] args) {
+        int n = 2030;
+        System.out.println("Welcome!!\n자바 세계로 오신 것을 환영합니다.");
+    }
+}
